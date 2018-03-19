64551240b7cd7f0b0fdf110a2e8708cb121920d4
Mario Härtwig <mario.haertwig@geops.de>
2018-03-19 15:05:41 +0100

    Updates test configuration and dependencies.

--- test/setup.js
+++ test/setup.js
@@ -1,12 +1,12 @@
-import ol from 'openlayers';
-global.ol = ol;
 import canvas from 'canvas';
 
-window.requestAnimationFrame = function(callback) {
+window.requestAnimationFrame = (callback) => {
   return setTimeout(callback, 1);
 };
-
 
 ['CanvasRenderingContext2D', 'CanvasPattern', 'CanvasGradient'].forEach(obj => {
   global[obj] = canvas[obj]
 });
+
+import ol from 'openlayers';
+global.ol = ol;
