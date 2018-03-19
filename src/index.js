0ca58354171ad5f529cb4efd0bc69293ddd97607
Friedjoff Trautwein <mail@friedjoff.de>
2018-03-19 09:48:17 +0100

    Move controls to control namespace.

--- src/index.js
+++ src/index.js
@@ -1,19 +1,4 @@
 import Editor from './editor';
-import Control from './control/control';
-import CadControl from './control/cad';
-import RotateControl from './control/rotate';
-import DrawControl from './control/draw';
-import MoveControl from './control/move';
-import ModifyControl from './control/modify';
-import DeleteControl from './control/delete';
+import * as control from './control';
 
-export {
-  Editor,
-  Control,
-  CadControl,
-  RotateControl,
-  DrawControl,
-  MoveControl,
-  ModifyControl,
-  DeleteControl,
-};
+export { control, Editor };
