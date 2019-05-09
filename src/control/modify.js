32c6d5bdae916b24ab4ca30621bfa4d5e949797f
Daniel Marsh-Hunn <daniel.marsh-hunn@geops.de>
2019-05-09 14:40:06 +0200

    added Select interaction, updated Delete and Modify to work only on selected features

--- src/control/modify.js
+++ src/control/modify.js
@@ -67,253 +67,183 @@ class ModifyControl extends Control {
       features: this.features,
     });
 
-    // const selected_features = this.selectInteraction.getFeatures();
-
-    if (options.style) {
-        // Apply the select style dynamically when the feature has its own style.
-        this.selectInteraction.getFeatures().on('add', (evt) => {
-
-          console.log(evt.element.getStyleFunction);
-          if (!evt.element.getStyleFunction()) {
-            console.log(evt.element);
-            return;
-          }
-
-          // Append the select style to the feature's style
-          const feature = evt.element;
-          const featureStyles = getStyles(feature.getStyleFunction());
-          const selectStyles = getStyles(options.style, feature);
-          const styles = featureStyles.concat(selectStyles);
-          evt.element.setStyle(styles);
-        });
-
-        // Remove the select style dynamically when the feature had its own style.
-      this.selectInteraction.getFeatures().on('remove', (evt) => {
-        if (!evt.element.getStyleFunction()) {
-          console.log("removed");
-          return;
-        }
-
-        // Remove the select styles
-        const feature = evt.element;
-        const styles = getStyles(feature.getStyleFunction(), null);
-        const selectStyles = getStyles(options.style, feature);
-        const featureStyles = styles.slice(0, styles.indexOf(selectStyles[0]));
-        evt.element.setStyle(featureStyles);
-      });
-    }
-
-      /**
-       * @type {ol.interaction.Modify}
-       * @private
-       */
-      this.modifyInteraction = new ol.interaction.Modify({
-        source: this.source,
-        style: options.modifyStyle,
-      });
-
-      /**
-       * @type {ol.interaction.Pointer}
-       * @private
-       */
-      this.moveInteraction = new ol.interaction.Pointer({
-        handleDownEvent: this.startMoveFeature.bind(this),
-        handleDragEvent: this.moveFeature.bind(this),
-        handleUpEvent: this.stopMoveFeature.bind(this),
-        handleMoveEvent: this.selectFeature.bind(this),
-      });
-
-    console.log(options);
-
-  }
-
-
-    activate() {
-      this.map.addInteraction(this.selectInteraction);
+    this.selectInteraction.getFeatures().on('add', (evt) => {
+      document.addEventListener('keydown', this.deleteFeature.bind(this));
       this.map.addInteraction(this.modifyInteraction);
-      super.activate();
-    }
+    })
+
+    this.selectInteraction.getFeatures().on('remove', (evt) => {
+      document.removeEventListener('keydown', this.deleteFeature.bind(this));
+      this.map.removeInteraction(this.modifyInteraction);
+    })
+
+    //-----------------------------------------------------------------
 
     /**
-     * @inheritdoc
+     * @type {ol.interaction.Modify}
+     * @private
      */
-    deactivate(silent) {
-      // this.selectInteraction.getFeatures().clear();
-      this.map.removeInteraction(this.selectInteraction);
-      this.map.removeInteraction(this.modifyInteraction);
-      super.deactivate(silent);
+    this.modifyInteraction = new ol.interaction.Modify({
+      // source: this.source,
+      features: this.selectInteraction.getFeatures(),
+      style: options.modifyStyle,
+    });
+
+    /**
+     * @type {ol.interaction.Pointer}
+     * @private
+     */
+    this.moveInteraction = new ol.interaction.Pointer({
+      handleDownEvent: this.startMoveFeature.bind(this),
+      handleDragEvent: this.moveFeature.bind(this),
+      handleUpEvent: this.stopMoveFeature.bind(this),
+      handleMoveEvent: this.selectFeature.bind(this),
+    });
+  }
+
+  /**
+   * Handle the event of the delete event listener.
+   * @param {Event} evt Event.
+   * @private
+   */
+  deleteFeature(evt) {
+    // console.log(this)
+    // console.log(this.selectInteraction.getFeatures().getArray().indexOf(this.feature));
+    if (evt.key === 'Delete' && this.selectInteraction.getFeatures()) {
+      for (var i = 0; i < this.selectInteraction.getFeatures().getArray().length; i++){
+        this.source.removeFeature(this.selectInteraction.getFeatures().getArray()[i]);
+      }
+      this.selectInteraction.getFeatures().clear()
     }
+  }
+
+  /**
+   * Handle the down event of the move interaction.
+   * @param {ol.MapBrowserEvent} evt Event.
+   * @private
+   */
+  startMoveFeature(evt) {
+    if (this.feature && this.modifyActive === false) {
+      if (this.feature.getGeometry() instanceof ol.geom.Point) {
+        const extent = this.feature.getGeometry().getExtent();
+        this.coordinate = ol.extent.getCenter(extent);
+      } else {
+        this.coordinate = evt.coordinate;
+      }
+      return true;
+    }
+
+    return false;
+  }
+
+  /**
+   * Handle the drag event of the move interaction.
+   * @param {ol.MapBrowserEvent} evt Event.
+   * @private
+   */
+  moveFeature(evt) {
+    if (this.modifyActive === false) {
+      const deltaX = evt.coordinate[0] - this.coordinate[0];
+      const deltaY = evt.coordinate[1] - this.coordinate[1];
+
+      this.feature.getGeometry().translate(deltaX, deltaY);
+      this.coordinate = evt.coordinate;
+    }
+  }
+
+  /**
+   * Handle the up event of the pointer interaction.
+   * @param {ol.MapBrowserEvent} evt Event.
+   * @private
+   */
+  stopMoveFeature() {
+    this.coordinate = null;
+    this.feature = null;
+    this.editor.setEditFeature(null);
+    return false;
+  }
+
+  /**
+   * Handle the move event of the move interaction.
+   * @param {ol.MapBrowserEvent} evt Event.
+   * @private
+   */
+  selectFeature(evt) {
+    if (this.feature) {
+      // Remove the select style dynamically when the feature had its own style.
+      if (this.feature.getStyleFunction()) {
+        const styles = getStyles(this.feature.getStyleFunction(), null);
+        const selectStyles = getStyles(this.selectStyle, this.feature);
+        this.feature.setStyle(styles.slice(0, styles.indexOf(selectStyles[0])));
+      }
+    }
+
+    this.feature = evt.map.forEachFeatureAtPixel(evt.pixel, (f) => {
+      if (this.source.getFeatures().indexOf(f) > -1) {
+        return f;
+      }
+      return null;
+    });
+
+    // Apply the select style dynamically when the feature has its own style.
+    if (this.feature && this.feature.getStyleFunction()) {
+      const featureStyles = getStyles(this.feature.getStyleFunction());
+      const selectStyles = getStyles(this.selectStyle, this.feature);
+      this.feature.setStyle(featureStyles.concat(selectStyles));
+    }
+
+    this.editor.setEditFeature(this.feature);
+    this.modifyActive = this.modifyInteraction.getOverlay()
+      .getSource().getFeatures().length > 0;
+
+    if (this.modifyActive) {
+      this.changeCursor('grab');
+    } else if (this.feature) {
+      this.changeCursor('move');
+    } else if (this.previousCursor !== null) {
+      this.changeCursor(this.previousCursor);
+      this.previousCursor = null;
+    }
+  }
+
+  /**
+   * Change cursor style.
+   * @param {string} cursor New cursor name.
+   * @private
+   */
+  changeCursor(cursor) {
+    const element = this.map.getTargetElement();
+    if (element.style.cursor !== cursor) {
+      if (this.previousCursor === null) {
+        this.previousCursor = element.style.cursor;
+      }
+      element.style.cursor = cursor;
+    }
+  }
+
+  /**
+   * @inheritdoc
+   */
+  activate() {
+    this.map.addInteraction(this.selectInteraction);
+    // document.addEventListener('keydown', this.deleteFeature.bind(this));
+    // this.map.addInteraction(this.modifyInteraction);
+    this.map.addInteraction(this.moveInteraction);
+    super.activate();
+  }
+
+  /**
+   * @inheritdoc
+   */
+  deactivate(silent) {
+    this.selectInteraction.getFeatures().clear();
+    this.map.removeInteraction(this.selectInteraction);
+    // document.removeEventListener('keydown', this.deleteFeature.bind(this));
+    // this.map.removeInteraction(this.modifyInteraction);
+    this.map.removeInteraction(this.moveInteraction);
+    super.deactivate(silent);
+  }
 }
 
 
 export default ModifyControl;
-
-    //-----------------------------------------------------------------
-//
-//     /**
-//      * @type {ol.interaction.Modify}
-//      * @private
-//      */
-//     this.modifyInteraction = new ol.interaction.Modify({
-//       source: this.source,
-//       style: options.modifyStyle,
-//     });
-//
-//     /**
-//      * @type {ol.interaction.Pointer}
-//      * @private
-//      */
-//     this.moveInteraction = new ol.interaction.Pointer({
-//       handleDownEvent: this.startMoveFeature.bind(this),
-//       handleDragEvent: this.moveFeature.bind(this),
-//       handleUpEvent: this.stopMoveFeature.bind(this),
-//       handleMoveEvent: this.selectFeature.bind(this),
-//     });
-//   }
-//
-//   /**
-//    * Handle the event of the delete event listener.
-//    * @param {Event} evt Event.
-//    * @private
-//    */
-//   deleteFeature(evt) {
-//     if (evt.key === 'Delete' && this.feature) {
-//       this.source.removeFeature(this.feature);
-//       this.feature = null;
-//     }
-//   }
-//
-//   /**
-//    * Handle the down event of the move interaction.
-//    * @param {ol.MapBrowserEvent} evt Event.
-//    * @private
-//    */
-//   startMoveFeature(evt) {
-//     if (this.feature && this.modifyActive === false) {
-//       if (this.feature.getGeometry() instanceof ol.geom.Point) {
-//         const extent = this.feature.getGeometry().getExtent();
-//         this.coordinate = ol.extent.getCenter(extent);
-//       } else {
-//         this.coordinate = evt.coordinate;
-//       }
-//       return true;
-//     }
-//
-//     return false;
-//   }
-//
-//   /**
-//    * Handle the drag event of the move interaction.
-//    * @param {ol.MapBrowserEvent} evt Event.
-//    * @private
-//    */
-//   moveFeature(evt) {
-//     if (this.modifyActive === false) {
-//       const deltaX = evt.coordinate[0] - this.coordinate[0];
-//       const deltaY = evt.coordinate[1] - this.coordinate[1];
-//
-//       this.feature.getGeometry().translate(deltaX, deltaY);
-//       this.coordinate = evt.coordinate;
-//     }
-//   }
-//
-//   /**
-//    * Handle the up event of the pointer interaction.
-//    * @param {ol.MapBrowserEvent} evt Event.
-//    * @private
-//    */
-//   stopMoveFeature() {
-//     this.coordinate = null;
-//     this.feature = null;
-//     this.editor.setEditFeature(null);
-//     return false;
-//   }
-//
-//   /**
-//    * Handle the move event of the move interaction.
-//    * @param {ol.MapBrowserEvent} evt Event.
-//    * @private
-//    */
-//   selectFeature(evt) {
-//     if (this.feature) {
-//       // Remove the select style dynamically when the feature had its own style.
-//       if (this.feature.getStyleFunction()) {
-//         const styles = getStyles(this.feature.getStyleFunction(), null);
-//         const selectStyles = getStyles(this.selectStyle, this.feature);
-//         this.feature.setStyle(styles.slice(0, styles.indexOf(selectStyles[0])));
-//       }
-//     }
-//
-//     this.feature = evt.map.forEachFeatureAtPixel(evt.pixel, (f) => {
-//       if (this.source.getFeatures().indexOf(f) > -1) {
-//         return f;
-//       }
-//       return null;
-//     });
-//
-//     // if (this.feature && this.feature.getStyleFunction()){
-//     //   this.feature.on('click', function(){
-//     //     console.log(this.feature);
-//     //   })
-//     // }
-//
-//     // Apply the select style dynamically when the feature has its own style.
-//     if (this.feature && this.feature.getStyleFunction()) {
-//       const featureStyles = getStyles(this.feature.getStyleFunction());
-//       const selectStyles = getStyles(this.selectStyle, this.feature);
-//       this.feature.setStyle(featureStyles.concat(selectStyles));
-//     }
-//
-//
-//     this.editor.setEditFeature(this.feature);
-//     this.modifyActive = this.modifyInteraction.getOverlay()
-//       .getSource().getFeatures().length > 0;
-//
-//     if (this.modifyActive) {
-//       this.changeCursor('grab');
-//     } else if (this.feature) {
-//       this.changeCursor('move');
-//     } else if (this.previousCursor !== null) {
-//       this.changeCursor(this.previousCursor);
-//       this.previousCursor = null;
-//     }
-//   }
-//
-//   /**
-//    * Change cursor style.
-//    * @param {string} cursor New cursor name.
-//    * @private
-//    */
-//   changeCursor(cursor) {
-//     const element = this.map.getTargetElement();
-//     if (element.style.cursor !== cursor) {
-//       if (this.previousCursor === null) {
-//         this.previousCursor = element.style.cursor;
-//       }
-//       element.style.cursor = cursor;
-//     }
-//   }
-//
-//   /**
-//    * @inheritdoc
-//    */
-//   activate() {
-//     document.addEventListener('keydown', this.deleteFeature.bind(this));
-//     this.map.addInteraction(this.modifyInteraction);
-//     this.map.addInteraction(this.moveInteraction);
-//     super.activate();
-//   }
-//
-//   /**
-//    * @inheritdoc
-//    */
-//   deactivate(silent) {
-//     document.removeEventListener('keydown', this.deleteFeature.bind(this));
-//     this.map.removeInteraction(this.modifyInteraction);
-//     this.map.removeInteraction(this.moveInteraction);
-//     super.deactivate(silent);
-//   }
-// }
-//
-//
-// export default ModifyControl;
