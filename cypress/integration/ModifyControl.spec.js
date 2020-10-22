006907d628885d09a585ebb7cf9e0d86b996f10c
Daniel <daniel.marsh-hunn@geops.de>
2020-10-22 11:32:32 +0200

    finished deleteNode test

---
+++ cypress/integration/ModifyControl.spec.js
@@ -0,0 +1,68 @@
+describe('ModifyControl', function () {
+  beforeEach(function () {
+    cy.visit('/');
+
+    // Draw polygon
+    cy.get('[title="Draw Polygon"]').click();
+    cy.get('.ol-overlaycontainer').click(100, 100, {force: true});
+    cy.get('.ol-overlaycontainer').click(100, 150, {force: true});
+    cy.get('.ol-overlaycontainer').click(150, 170, {force: true});
+    cy.get('.ol-overlaycontainer').dblclick(200, 100, {force: true});
+
+    // Draw line
+    cy.get('[title="Draw LineString"]').click();
+    cy.get('.ol-overlaycontainer').click(400, 350, {force: true});
+    cy.get('.ol-overlaycontainer').click(270, 344, {force: true});
+    cy.get('.ol-overlaycontainer').dblclick(200, 450, {force: true});
+  });
+
+  it('should correctly handle node deletion', function () {
+    cy.window().then((win) => {
+      // Spy on selectModify.addFeatureLayerAssociation_, called when a feature is selected
+      const omitFeatureSelectSpy = cy.spy(win.modify.selectModify, 'addFeatureLayerAssociation_');
+      let selectedFeaturesArray = [];
+      // Select polygon
+      cy.get('.ole-control-modify').click();
+      cy.get('.ol-overlaycontainer').dblclick(100, 100, {force: true}).then(() => {
+        selectedFeaturesArray = win.modify.selectModify.getFeatures().getArray();
+        // Check if only one feature is selected
+        expect(selectedFeaturesArray.length).to.equal(1);
+        // Verify the polygon has 4 nodes (5 coordinates)
+        expect(selectedFeaturesArray[0].getGeometry().getCoordinates()[0].length).to.equal(5);
+      });
+      // Click & delete a node
+      cy.get('.ol-overlaycontainer').click(102, 152, {force: true}).then(() => {
+        // Verify one polygon node was deleted on click (3 nodes, 4 coordinates)
+        expect(selectedFeaturesArray[0].getGeometry().getCoordinates()[0].length).to.equal(4);
+      });
+      // Click another node
+      cy.get('.ol-overlaycontainer').click(100, 100, {force: true}).then(() => {
+        // Verify no further node was deleted on click (because polygon minimum number nodes is 3)
+        expect(selectedFeaturesArray[0].getGeometry().getCoordinates()[0].length).to.equal(4);
+        // Check that no features from the overlay are mistakenly selected
+        expect(omitFeatureSelectSpy.withArgs(omitFeatureSelectSpy.args[0][0], null)).to.not.be.called
+      });
+
+      // Select line
+      cy.get('.ol-overlaycontainer').dblclick(270, 344, {force: true}).then(() => {
+        selectedFeaturesArray = win.modify.selectModify.getFeatures().getArray();
+        // Check if only one feature is selected
+        expect(selectedFeaturesArray.length).to.equal(1);
+        // Verify the line has 3 nodes (3 coordinates)
+        expect(selectedFeaturesArray[0].getGeometry().getCoordinates().length).to.equal(3);
+      });
+      // Click & delete a node
+      cy.get('.ol-overlaycontainer').click(270, 344, {force: true}).then(() => {
+        // Verify one line node was deleted on click (2 nodes, 2 coordinates)
+        expect(selectedFeaturesArray[0].getGeometry().getCoordinates().length).to.equal(2);
+      });
+      // Click another node
+      cy.get('.ol-overlaycontainer').click(400, 350, {force: true}).then(() => {
+        // Verify no further node was deleted on click (because polygon minimum number nodes is 2)
+        expect(selectedFeaturesArray[0].getGeometry().getCoordinates().length).to.equal(2);
+        // Check that no features from the overlay are mistakenly selected
+        expect(omitFeatureSelectSpy.withArgs(omitFeatureSelectSpy.args[0][0], null)).to.not.be.called
+      });
+    });
+  });
+});
