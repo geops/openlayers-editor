cf9d2f894cf1e7a782b55ddde92d5ceaa2bf8e0d
Olivier Terral <olivier.terral@geops.de>
2020-11-06 14:55:38 +0100

    Add precommit for api keys

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -3,7 +3,8 @@ const { start } = require('neutrino');
 const library = require('@neutrinojs/library');
 const imageLoader = require('@constgen/neutrino-svg-loader');
 const styleLoader = require('@neutrinojs/style-loader');
-
+// 5cc87b12d7c5370001c1d655e96f1d54c5404bbb8cc4cfcd2ff5111b5
+// 5cc87b12d7c5370001c1d655352830d2fef24680ae3a1cda54418cb8
 module.exports = {
   options: {
     root: __dirname,
