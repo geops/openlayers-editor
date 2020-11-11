2222aaac49c93163077d4a0914118755a5b742d4
Olivier Terral <olivier.terral@geops.de>
2020-11-11 11:07:21 +0100

    fix(selectmove): add a default style with a high zIndex to selectmove interaction

--- src/interaction/selectmove.js
+++ src/interaction/selectmove.js
@@ -1,5 +1,25 @@
 import Select from 'ol/interaction/Select';
+import { Circle, Style, Fill, Stroke } from 'ol/style';
 import { click, shiftKeyOnly } from 'ol/events/condition';
+
+// Default style on moving geometries
+const selectMoveStyle = new Style({
+  zIndex: 10000, // Always on top
+  image: new Circle({
+    radius: 5,
+    fill: new Fill({
+      color: '#05A0FF',
+    }),
+    stroke: new Stroke({ color: '#05A0FF', width: 2 }),
+  }),
+  stroke: new Stroke({
+    color: '#05A0FF',
+    width: 3,
+  }),
+  fill: new Fill({
+    color: 'rgba(255,255,255,0.4)',
+  }),
+});
 
 /**
  * Select features for modification by a Move interaction.
@@ -18,6 +38,7 @@ class SelectMove extends Select {
       condition: click,
       toggleCondition: shiftKeyOnly,
       wrapX: false,
+      style: selectMoveStyle,
       ...options,
     });
   }
