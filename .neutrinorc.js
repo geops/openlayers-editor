ed02fd1ca8c3a035498e05e18c97594f0a000258
Olivier Terral <olivier.terral@geops.com>
2023-04-12 11:06:21 +0200

    chore: remove all useless libs

--- .neutrinorc.js
+++ .neutrinorc.js
@@ -11,29 +11,6 @@ module.exports = {
   use: [
     library({
       name: 'ole',
-      babel: {
-        presets: [
-          [
-            '@babel/preset-env',
-            {
-              targets: {
-                browsers: [
-                  '>0.2%',
-                  'not dead',
-                  'not op_mini all',
-                  'not ie <= 11',
-                  'not android < 5',
-                ],
-              },
-            },
-          ],
-        ],
-        plugins: [
-          '@babel/plugin-transform-destructuring',
-          '@babel/plugin-transform-object-assign',
-          '@babel/plugin-proposal-object-rest-spread',
-        ],
-      },
     }),
     imageLoader(),
     styleLoader({ extract: false }),
--- cypress/integration/control/draw.spec.js
+++ cypress/integration/control/draw.spec.js
@@ -1,3 +1,4 @@
+/* eslint-disable import/no-named-as-default */
 import DrawControl from '../../../src/control/draw';
 
 describe('Draw control', () => {
