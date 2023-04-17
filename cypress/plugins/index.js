aecf548ef489244a913850a4816b458f75ef1c39
Friedjoff Trautwein <friedjoff.trautwein@geops.com>
2023-04-17 12:42:42 +0200

    chore: switch from Neutrino (webpack) to esbuild for dev server

--- cypress/plugins/index.js
+++ cypress/plugins/index.js
@@ -1,22 +1,22 @@
-const webpack = require('webpack');
-const webpackPreprocessor = require('@cypress/webpack-preprocessor');
-const webpackConfig = require('../../webpack.config');
+/// <reference types="cypress" />
+// ***********************************************************
+// This example plugins/index.js can be used to load plugins
+//
+// You can change the location of this file or turn off loading
+// the plugins file with the 'pluginsFile' configuration option.
+//
+// You can read more here:
+// https://on.cypress.io/plugins-guide
+// ***********************************************************
 
-// const Neutrino = require('neutrino/Neutrino');
+// This function is called when a project is opened or re-opened (e.g. due to
+// the project's config changing)
 
-module.exports = (on) => {
+/**
+ * @type {Cypress.PluginConfig}
+ */
+// eslint-disable-next-line no-unused-vars
+module.exports = (on, config) => {
   // `on` is used to hook into various events Cypress emits
   // `config` is the resolved Cypress config
-  // const options = webpackPreprocessor.defaultOptions;
-  // const olProvider = new webpack.ProvidePlugin({ ol: 'openlayers' });
-  // options.webpackOptions.plugins = [olProvider];
-
-  const options = {
-    // send in the options from your webpack.config.js, so it works the same
-    // as your app's code
-    webpackOptions: webpackConfig,
-    watchOptions: {},
-  };
-
-  on('file:preprocessor', webpackPreprocessor(options));
-};
+}
